75b52bae29b1bf7a7a27c6bbb1076b9d5c775c82
Shree haran <shreeharan2001@gmail.com>
2023-02-12 18:51:32 +0530

    Responsive Navbar

--- src/Components/UI/Navbar.js
+++ src/Components/UI/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import MenuItems from './MenuItems'
@@ -6,11 +6,20 @@ import MenuItems from './MenuItems'
 import classes from './Navbar.module.css'
 
 const Navbar = () => {
+    const [changeIcon, setChangeIcon] = useState(false);
+    const iconChangeHandler = () => {
+        setChangeIcon(!changeIcon);
+    }
     return (
         <header>
             <nav className={classes.navbarItems}>
                 <h1 className={classes.navbarLogo}>Travel</h1>
-                <ul className={classes.navItems}>
+
+                <div className={classes.menuIcon} onClick={iconChangeHandler}>
+                    <i className={changeIcon ? "fas fa-times" : "fas fa-bars"}></i>
+                    {/* <i className="fas fa-times"></i> */}
+                </div>
+                <ul className={!changeIcon ? classes.navItems : classes.navItems + ' ' + classes.active}>
                     {MenuItems.map((item, index) => {
                         return (
                             <li key={index}>
